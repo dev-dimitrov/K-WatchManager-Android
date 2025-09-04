d98cae20b69164a7929b3752b203b17780254882
su-kaizen <badboynodo313@gmail.com>
2025-09-04 12:27:01 +0200

    feature(add-watch-screen): The info of every watch is now showing correctly, the last adjustment, the checkAccuracy button and the adjust button are not working yet

--- app/src/main/java/kaizen/watchmanager/WatchScreenActivity.kt
+++ app/src/main/java/kaizen/watchmanager/WatchScreenActivity.kt
@@ -1,8 +1,11 @@
 package kaizen.watchmanager
 
 import Watch
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +14,11 @@ import androidx.core.view.WindowInsetsCompat
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class WatchScreenActivity : AppCompatActivity() {
+    lateinit var brandTxt: TextView;
+    lateinit var modelTxt: TextView;
+    lateinit var adjustBtn: Button;
+    lateinit var accBtn: Button;
+    lateinit var watchAtt: TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,7 +28,24 @@ class WatchScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var selectedWatch: Watch?;
-        selectedWatch = intent.getParcelableExtra("WATCH", Watch::class.java)
+        asignObjectId();
+        var selectedWatch: Watch;
+        selectedWatch = intent.getParcelableExtra("WATCH", Watch::class.java)!!;
+        showWatchInfo(selectedWatch);
+    }
+
+    fun asignObjectId(){
+        brandTxt = findViewById(R.id.brandText);
+        modelTxt = findViewById(R.id.modelText);
+        adjustBtn = findViewById(R.id.adjustButton);
+        brandTxt = findViewById(R.id.brandText);
+        accBtn = findViewById(R.id.checkButton);
+        watchAtt = findViewById(R.id.attText);
+    }
+
+    fun showWatchInfo(w: Watch){
+        brandTxt.text = w.brand;
+        modelTxt.text = w.model;
+        watchAtt.text = "Movement type: "+w.type+"\nCaliber: "+w.caliber+"\nTheoretic accuracy: "+w.theoreticAccuracy+"\nMore information: "+w.moreInfo;
     }
 }
