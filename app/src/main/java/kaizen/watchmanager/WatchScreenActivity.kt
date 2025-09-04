a6477ec892c1ee4a9e48224295d05dd6bf932184
su-kaizen <badboynodo313@gmail.com>
2025-09-04 12:10:35 +0200

    feature(add-watch-screen): Added the layout and style for the watch screen. Added too the main logic tomove the watch object from the main activity to the watch screen activity

---
+++ app/src/main/java/kaizen/watchmanager/WatchScreenActivity.kt
@@ -0,0 +1,26 @@
+package kaizen.watchmanager
+
+import Watch
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+class WatchScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_watch_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var selectedWatch: Watch?;
+        selectedWatch = intent.getParcelableExtra("WATCH", Watch::class.java)
+    }
+}
