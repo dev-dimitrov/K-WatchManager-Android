5964b2864bb7b32e26de38b629b5493cd9fdd46b
su-kaizen <badboynodo313@gmail.com>
2025-09-18 18:45:32 +0200

    feature(prepare-watch-logs): Prepared the watch class with some methods to make it easier to implement

--- app/src/main/java/kaizen/watchmanager/Watch.kt
+++ app/src/main/java/kaizen/watchmanager/Watch.kt
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 import java.util.TreeMap
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 class Watch( // constructor parameters
     b: String,
     m: String,
@@ -25,7 +25,7 @@ class Watch( // constructor parameters
     var theoreticAccuracy: String;
     var moreInfo: String;
     var lastAdjust = "";
-    var log = "";
+    var log = LinkedHashMap<String,String>();
     var na = "Not Specified";
 
     init {
@@ -35,13 +35,27 @@ class Watch( // constructor parameters
         caliber = if(c.isEmpty()) na else c;
         theoreticAccuracy = if(t.isEmpty()) na else t;
         moreInfo = if(mi.isEmpty()) na else mi;
+        log = LinkedHashMap();
     }
-
 
     override fun toString(): String{
         return brand+" "+model;
     }
 
+    fun logWrite(desc: String){
+        // Format the exact time of log creation to string
+        var parsedDate = LocalDateTime.now().format(formatter);
+        log.put(parsedDate,desc);
+    }
+
+    fun getLog(): String{
+        var keyS = log.keys;
+        var s = "";
+        for(k in keyS){
+            s+= k+" | "+log.get(k)+"\n";
+        }
+        return s; // return all the logs in String.
+    }
 
     override fun equals(other: Any?): Boolean {
         var o = other as Watch;
@@ -49,7 +63,6 @@ class Watch( // constructor parameters
     }
 
     companion object{
-        @RequiresApi(Build.VERSION_CODES.O)
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     }
 }
