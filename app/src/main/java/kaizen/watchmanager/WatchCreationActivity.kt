497f504243b0aeceb6a9b43072c78bac64b567b1
David Dimitrov <david.dimitrov@educa.madrid.org>
2026-01-11 20:07:44 +0100

    Added the code in order to work the combo box thing for the caliber type in the watch creation activity

--- app/src/main/java/kaizen/watchmanager/WatchCreationActivity.kt
+++ app/src/main/java/kaizen/watchmanager/WatchCreationActivity.kt
@@ -6,7 +6,9 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
@@ -26,6 +28,7 @@ class WatchCreationActivity : AppCompatActivity() {
     private lateinit var addWatch: Button;
     private lateinit var statusText: TextView;
     private lateinit var cancel: Button;
+    private lateinit var spinner: Spinner;
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +48,21 @@ class WatchCreationActivity : AppCompatActivity() {
         statusText = findViewById(R.id.statusTxt);
         theoreticAccuracyI = findViewById(R.id.taInput);
         cancel = findViewById(R.id.cancelButton);
+
+        // Stuff for the combo box... In android is called "Spinner" (lmao)
+        spinner = findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.caliber_type,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
 
         statusText.visibility = View.INVISIBLE;
 
